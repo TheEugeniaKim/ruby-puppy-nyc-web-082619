f6f2b712aaaa3817efa3a74dae14233d288cf186
Eugenia Kim <elk303@nyu.edu>
2019-08-28 20:20:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+  attr_reader :name, :save 
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+    @save = save 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end
+    
+  def self.print_all
+    @@all.each do |puppy|
+      puts puppy.name 
+    end
+  end
+  
+  def self.save 
+    
+  end 
+end
